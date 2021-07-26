5ea61f0847fec500aa674d79e512f9320ced9196
Elena Petrova <lazy.flo@gmail.com>
2021-07-26 16:52:58 +0300

    модальное окно с меню

--- one.js
+++ one.js
@@ -1,2 +1,21 @@
-var vowels = [а, у, о, ы, и, э, я, ю, ё, е];
-console.log(vowels[3]);
+const modalMenu = document.querySelector('#modalMenu');
+console.log(modalMenu);
+const header = document.querySelector('.header')
+console.log(header);
+const menu = document.querySelector('.menu--vertical');
+const modalBtn = document.querySelector('#hamburger-menu');
+console.log(modalBtn);
+const close = document.querySelector('.fullscreen-menu__close');
+
+modalBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+modalMenu.style.display = "block"
+header.style.position = "relative"
+menu.style.display = "block"
+});
+
+close.addEventListener ('click', (e) => {
+  modalMenu.style.display = "none"
+  header.style.position = "absolute"
+  menu.style.display = "none"
+});
