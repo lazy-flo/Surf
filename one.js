99c47b8eaa02a626552a83e20b96fcd224069252
Elena Petrova <lazy.flo@gmail.com>
2021-07-28 18:42:37 +0300

    мобильная версия для телефона

--- one.js
+++ one.js
@@ -7,15 +7,33 @@ const modalBtn = document.querySelector('#hamburger-menu');
 console.log(modalBtn);
 const close = document.querySelector('.fullscreen-menu__close');
 
+const menuList = modalMenu.querySelector('.menu__list');
+
+function openMenu() {
+  modalMenu.style.display = "block";
+  header.style.position = "relative";
+  menu.style.display = "block";
+}
+
+function closeMenu(){
+  modalMenu.style.display = "none";
+  header.style.position = "absolute";
+  menu.style.display = "none";
+}
+
+menuList.addEventListener('click', (e) => {
+  e.preventDefault();
+  if(e.target.classList.contains('menu__link')){
+    closeMenu();
+  };
+})
+
 modalBtn.addEventListener('click', (e) => {
   e.preventDefault()
-modalMenu.style.display = "block"
-header.style.position = "relative"
-menu.style.display = "block"
+  openMenu();
 });
 
 close.addEventListener ('click', (e) => {
-  modalMenu.style.display = "none"
-  header.style.position = "absolute"
-  menu.style.display = "none"
+  e.preventDefault();
+ closeMenu()
 });
